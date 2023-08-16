8c605f611a385f2875c2de8bd8f5af42656fdd52
abircu <mdabircse4873@gmail.com>
2023-08-16 17:52:54 +0600

    recap is complete

--- index.js
+++ index.js
@@ -11,7 +11,45 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   const getDepositString = getDeposittext.innerText;
   const depositNumber = parseFloat(getDepositString);
 
-  //   step 03: Addition input value of deposit box
+  //   step 03: Add input value of deposit box
   const addDeposit = inputNumber + depositNumber;
   getDeposittext.innerText = addDeposit;
+
+  //   step 04: upgrade balance calculation
+  const balanceElement = document.getElementById("Balance-total");
+  const balaceTotalElementString = balanceElement.innerText;
+  const balaceTotal = parseFloat(balaceTotalElementString);
+
+  //   step 05:Total balance add of input field
+  const totalBalanceAdd = balaceTotal + inputNumber;
+  balanceElement.innerText = totalBalanceAdd;
+});
+
+// withdraw button functionality here
+// step 01:withdraw button clickable function
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  //  step 02:withdraw input  functionality
+  const withdrawInputElement = document.getElementById("withdraw-amount");
+  const withdrawInputElementString = withdrawInputElement.value;
+  //   step 2.5:convert withdraw inpu text to number
+  const withdrawAmountNumber = parseFloat(withdrawInputElementString);
+  withdrawInputElement.value = "";
+
+  //   step 03:withdraw box functionality
+  const withdrawBoxElement = document.getElementById("Withdraw-total");
+  const withdrawBoxElementString = withdrawBoxElement.innerText;
+  const withdrawAmountTotal = parseFloat(withdrawBoxElementString);
+
+  //   step 04:withdraw amount add on withdraw box
+  const additionWithdraw = withdrawAmountNumber + withdrawAmountTotal;
+  withdrawBoxElement.innerText = additionWithdraw;
+
+  // step 05:balace box functionality
+  const balanceBoxElement = document.getElementById("Balance-total");
+  const balanceBoxElementString = balanceBoxElement.innerText;
+  const balaceTotalElementNumber = parseFloat(balanceBoxElementString);
+
+  //   step 06:calculation final step of substruction of withdraw amount
+  const withdrawAmountSub = balaceTotalElementNumber - withdrawAmountNumber;
+  balanceBoxElement.innerText = withdrawAmountSub;
 });
