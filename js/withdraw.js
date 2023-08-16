8c818ea793c0b8e2b14d878ecba419d41c003ed2
abircu <mdabircse4873@gmail.com>
2023-08-16 23:48:28 +0600

    withdraw button also added

---
+++ js/withdraw.js
@@ -0,0 +1,9 @@
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const withdrawInputField = getInputValueById("withdraw-amount");
+  const withdrawTextBox = getInputTextById("Withdraw-total");
+  const withdrawBalanceTotal = getInputTextById("Balance-total");
+  const addWithdrawAmount = withdrawInputField + withdrawTextBox;
+  const totalBalance = withdrawBalanceTotal - withdrawInputField;
+  setTextElementByValue("Withdraw-total", addWithdrawAmount);
+  setTextElementByValue("Balance-total", totalBalance);
+});
